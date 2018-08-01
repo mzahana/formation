76872afbda7cdb1fb29a4e86dd864c441df8e1fc
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2018-08-01 20:16:15 +0300

    change ROS_HOSTNAME=localhost in startup_script.sh

--- scripts/startup_launch.sh
+++ scripts/startup_launch.sh
@@ -5,7 +5,7 @@ source /home/odroid/catkin_ws/devel/setup.bash
 # If you are using telem radio to communicate with GCS, comment the following line
 #export ROS_MASTER_URI=http://192.168.0.105:11311
 #ip=`ifconfig wlan0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
-export ROS_HOSTNAME=$(hostname -I)
-roslaunch formation start_robot_and_serial_nodes.launch
+export ROS_HOSTNAME=localhost
+roslaunch formation start_robot_and_serial_nodes.launch id:=0 indoor:=false rbanme:=drone1
 # Comment the above line, and uncomment the following if you are using serial telem radio
 #roslaunch formation start_robot_and_serial_nodes
